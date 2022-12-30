a4ca9db019a241219042e7ec2bf018957916b74f
Данила <danilapetrov522@gmail.com>
2022-12-30 14:26:58 +0400

    second commit

--- src/test/java/com/elseff/project/ProjectApplicationTests.java
+++ src/test/java/com/elseff/project/ProjectApplicationTests.java
@@ -11,7 +11,7 @@ class ProjectApplicationTests {
         int number1 = 5;
         int number2 = 10;
         int sum = number1 + number2;
-        Assertions.assertEquals(16, sum);
+        Assertions.assertEquals(17, sum);
     }
 
 }
